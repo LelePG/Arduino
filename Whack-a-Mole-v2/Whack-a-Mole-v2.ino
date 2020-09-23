c322b5c5564959dd5df8987f1b62f2d2d847321e
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2020-09-23 20:58:40 -0300

    Adicionado projeto de tranca com teclado de Membrana e Servo

--- Whack-a-Mole-v2/Whack-a-Mole-v2.ino
+++ Whack-a-Mole-v2/Whack-a-Mole-v2.ino
@@ -1,16 +1,16 @@
 /*
    Segunda versão do jogo Whack-a-Mole, utilizando um LED RGB e um controle de módulo Joystick. São 5 cores de ordem, segundo o disposto abaixo:
-  
+
    Vermelho - Movimentação em X-
    Azul - Movimentação em X+
    Verde - Movimentação em Y-
    Lilás - Movimentação em Y+
    Amarelo - Aperto do botão
-   
-   Após a luz acender, a ação deve ser tomada em um intervalo de tempo para a contabilização de pontos. Com três erros, o jogo é perdido, e ganho com 10. Estes valores podem
-   ser vistos no monitor Serial ao final do jogo. O tempo para performar a ação é o tempo que o LED fica aceso,e pode ser definido na variável tempo. 
 
-   
+   Após a luz acender, a ação deve ser tomada em um intervalo de tempo para a contabilização de pontos. Com três erros, o jogo é perdido, e ganho com 10. Estes valores podem
+   ser vistos no monitor Serial ao final do jogo. O tempo para performar a ação é o tempo que o LED fica aceso,e pode ser definido na variável tempo.
+
+
    Letícia Pegoraro Garcez 2020
 */
 
@@ -25,7 +25,7 @@
 
 #define buzzer 8
 
-int cor[5][3] = {{0, 0, 255}, {255, 0, 0}, {25,200,56}, {50,150,200}, {0,0,0} } ; //Cores que o LED pode assumir
+int cor[5][3] = {{0, 0, 255}, {255, 0, 0}, {25, 200, 56}, {50, 150, 200}, {0, 0, 0} } ; //Cores que o LED pode assumir
 int escolha, erros, pontos;
 int tempo = 1500; //Tempo em que o LED fica ligado
 
@@ -38,8 +38,8 @@ void setup() {
   pinMode(ledR, OUTPUT);
   pinMode(ledG, OUTPUT);
   pinMode(ledB, OUTPUT);
-  pinMode(buzzer,OUTPUT);
-  
+  pinMode(buzzer, OUTPUT);
+
   pinMode(eixoX, INPUT);
   pinMode(eixoY, INPUT);
   pinMode(botao, INPUT);
@@ -47,12 +47,8 @@ void setup() {
 }
 
 void loop() {
- // for (int c=0;c<6;c++){
- //   setColorLed(cor[c]);
- //   delay(1000);
- // }
-  
- randomSeed(millis());
+ 
+  randomSeed(millis());
   escolha = random(4);//Seleciona uma das cores
   setColorLed(cor[escolha]);//aplica essa cor
   delay(tempo);//deixa o LED aceso
@@ -60,12 +56,12 @@ void loop() {
   setColorLed(cor[4]);//desliga o LED
   delay(300);
 
-  if(pontos >= 10){//Se o jogo é ganhado
+  if (pontos >= 10) { //Se o jogo é ganhado
     Serial.println("Você venceu!");
     pontos = 0;
     erros = 0;
   }
-  else if(erros>=3){//Se o jogo foi perdido.
+  else if (erros >= 3) { //Se o jogo foi perdido.
     Serial.println("Você perdeu!");
     pontos = 0;
     erros = 0;
@@ -102,14 +98,14 @@ void verificaAcao(int n) {
   }
 }
 
-void play_acerto(void){
+void play_acerto(void) {
   tone(buzzer, 1500);
   delay(600);
   noTone(buzzer);
   pontos++;
 }
 
-void play_erro(void){
+void play_erro(void) {
   tone(buzzer, 1750);
   delay(600);
   noTone(buzzer);
